f7e18ecc22a5c6d7059065c33bfa7ea17d0f2391
itspoma <itspoma@gmail.com>
2017-06-07 17:55:26 +0300

    testing github deploy (0.3)

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-    res.send('Hello from inside a container 0.3');
+    res.send('Hello from inside a container 0.4');
 });
 
 app.listen(8080);
