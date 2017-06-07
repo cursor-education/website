8bf5252bae84e50d0e570741a98c318c908d69d5
itspoma <itspoma@gmail.com>
2017-06-07 18:33:28 +0300

    test update for deployment (v0.6)

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-    res.send('Hello from inside a container 0.5');
+    res.send('Hello from inside a container 0.6');
 });
 
 app.listen(8080);
